de6a4dc89bece7602a54d7b717123d76ce369ca3
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-12 15:58:24 +0200

    update file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,13 @@ import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div className=''></div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((tecnology)=>(
+        <div className='w-28 h-28' key={tecnology.name}>
+         <BallCanvas icon={tecnology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
