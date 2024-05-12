023d58d10e33f8752fb1ae5351b3eb89ec017dce
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-12 17:49:27 +0200

    update file

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { styles } from '../style';
+import { github } from '../assets';
+import { SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+textVariant
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+     <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, '');
