adcdeb7873a41d3f719a80597abd4797497d264e
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-12 16:49:50 +0200

    update file

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -17,19 +17,20 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
+        <meshStandardMaterial  color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
+        <Decal
+        position={[0,0,1]} rotation={[2 * Math.PI  , 0, 6.25]} map={decal} flatShading />
       </mesh>
     </Float>
   );
 };
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
