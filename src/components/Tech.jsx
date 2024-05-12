d5fe8ad2d4bd2971c89eb159c266a571bbb445ec
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-12 15:54:38 +0200

    update file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className=''></div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, '');
