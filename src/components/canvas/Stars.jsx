4e2d97f5ffddd5808ae48ccbf839b01f57846927
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-13 14:26:28 +0200

    update file

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,9 +6,13 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  useFrame ((state, delta)=>{
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  })
   return (
-    <group>
-      <points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+    <group rotation={[0,0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
@@ -16,16 +20,16 @@ const Stars = (props) => {
           sizeAttenuation={true}
           depthWrite={false}
         />
-      </points>
+      </Points>
     </group>
   );
 };
 const StarCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
-          <stars />
+          <Stars />
         </Suspense>
         <Preload all />
       </Canvas>
