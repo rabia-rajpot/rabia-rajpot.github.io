1a54206ee47e77cbe4c2658550cc1cb4524bb4ae
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-13 13:26:31 +0200

    update file

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef, useState } from "react";
+import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
+const Stars = (props) => {
+  return <div>Stars</div>;
+};
+const StarCanvas = () => {
   return (
-    <div>Stars</div>
-  )
-}
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <stars />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
 
-export default Stars
+export default StarCanvas;
