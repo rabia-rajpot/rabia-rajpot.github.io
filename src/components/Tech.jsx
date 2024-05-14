c53741c8f51da0e7eb68de5e75b8d3ded03bf2f9
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-14 17:23:37 +0200

    update file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,17 +3,18 @@ import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
-
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((tecnology)=>(
-        <div className='w-28 h-28' key={tecnology.name}>
-         <BallCanvas icon={tecnology.icon} />
+    <div className='flex flex-wrap justify-center gap-20' style={{ flexWrap: 'wrap' }}>
+      {technologies.map((technology, index) => (
+        <div key={technology.name} style={{ marginBottom: '10px', marginRight: index % 4 === 3 ? '0' : '20px' }}>
+          <div className='w-28 h-28'>
+            <BallCanvas icon={technology.icon} />
+          </div>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default SectionWrapper(Tech, '');
