56d07124c838f0eb1e1120c1b6508c0b03445648
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-14 14:53:34 +0200

    update file

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -51,7 +51,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="always"
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
