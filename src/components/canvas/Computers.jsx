91f3053ad5de12a02a458a2dceb5b70ccdc8b198
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-14 14:33:51 +0200

    update file

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,12 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
-
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
   return (
     <mesh>
-      <hemisphereLight intensity={Math.PI} groundColor='black' />
+      <hemisphereLight intensity={3} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -22,13 +19,12 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -2.60, -1]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
-
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
@@ -55,7 +51,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="always"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
