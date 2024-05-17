4a287ec46dfedd2609865a2d51d91feee132b301
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-17 15:12:03 +0200

    update file

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import { BallCanvas } from './canvas';
-import { SectionWrapper } from '../hoc';
-import { technologies } from '../constants';
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-wrap justify-center gap-20' style={{ flexWrap: 'wrap' }}>
+    <div
+      className="flex flex-wrap justify-center gap-20"
+      style={{ flexWrap: "wrap" }}
+    >
       {technologies.map((technology, index) => (
-        <div key={technology.name} style={{ marginBottom: '10px', marginRight: index % 4 === 3 ? '0' : '20px' }}>
-          <div className='w-28 h-28'>
+        <div
+          key={technology.name}
+          style={{
+            marginBottom: "10px",
+            marginRight: index % 4 === 3 ? "0" : "20px",
+          }}
+        >
+          <div className="w-28 h-28">
             <BallCanvas icon={technology.icon} />
           </div>
         </div>
       ))}
     </div>
   );
-}
+};
 
-export default SectionWrapper(Tech, '');
+export default SectionWrapper(Tech, "");
