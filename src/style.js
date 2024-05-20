a063728cc53ab7918d4cd65c50276716c4ca206a
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-20 13:09:09 +0200

    update file

--- src/style.js
+++ src/style.js
@@ -12,6 +12,8 @@ const styles = {
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
       "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+      sectionHeadsText:
+      "text-white font-black mt-4 md:text-[45px] sm:text-[50px] xs:text-[40px] text-[30px]",
   };
   
   export { styles };
